22336421222aa182b1522e6eead35cc2a1876025
xHyroM <generalkubo@gmail.com>
2022-09-02 18:31:12 +0200

    fix(injector): directory search on windows

--- packages/injector/src/os/windows.zig
+++ packages/injector/src/os/windows.zig
@@ -18,13 +18,21 @@ pub fn get_app_directory(platform: discord_platform) string {
         else => searched = std.fmt.allocPrint(allocator, "{s}/DiscordDevelopment", .{localAppData}) catch unreachable,
     }
 
-    if (searched != null) return searched.?;
-
     while (searched == null or std.os.system.access(allocator.dupeZ(u8, searched.?) catch unreachable, std.os.F_OK) != 0) {
         Logger.err("Failed to locate discord {s} installation folder.", .{@tagName(platform)});
         Logger.infoNoBreak("Please, write your discord path: ", .{});
         searched = std.io.getStdIn().reader().readUntilDelimiterAlloc(allocator, '\n', 1024) catch unreachable;
     }
 
-    return join_path(&.{searched, "resources", "app"}) catch unreachable;
+    const dir = std.fs.cwd().openDir(".", .{ .iterate = true, }) catch unreachable;
+    var iter = dir.iterate();
+    var app_version_dir = "";
+
+    while (try iter.next()) |app_dir| {
+        if (app_dir.kind != std.fs.File.Kind.Directory or std.mem.indexOf(u8, app_dir.name, "app") == null) continue;
+
+        app_version_dir = app_dir.name;
+    }
+
+    return join_path(&.{searched, app_version_dir, "resources", "app"});
 }
