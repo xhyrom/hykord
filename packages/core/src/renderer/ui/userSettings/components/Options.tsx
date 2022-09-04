1efab429fc25d5e0e4978074430c5d86695e5a85
xHyroM <generalkubo@gmail.com>
2022-09-04 15:10:36 +0200

    feat(core): custom modal in bypass_security_check_require option

--- packages/core/src/renderer/ui/userSettings/components/Options.tsx
+++ packages/core/src/renderer/ui/userSettings/components/Options.tsx
@@ -1,7 +1,7 @@
 import { React } from "@hykord/webpack";
 import { Boolean } from '@hykord/components/inputs';
 import { FormTitle, FormItem, ErrorBoundary } from '@hykord/components';
-import { openRequiredRestartModal } from '@hykord/utilities/modals';
+import { openConfirmationModal, openRequiredRestartModal } from '@hykord/utilities/modals';
 
 export default async() => {
     const settings = window.hykord.settings;
@@ -28,11 +28,28 @@ export default async() => {
                 </FormItem>
                 <FormItem title={"Hykord Options"} noteHasMargin>
                     <Boolean
-                        toggle={() => settings.toggleSetting("hykord.bypass_security_check_require")}
+                        toggle={() => {
+                            return new Promise((resolve) => {
+                                if (settings.getSetting('hykord.bypass_security_check_require')) return resolve(settings.toggleSetting("hykord.bypass_security_check_require"));
+
+                                openConfirmationModal(
+                                    {
+                                        header: 'Additional Info',
+                                        confirmText: 'Accept',
+                                        cancelText: 'Cancel',
+                                        content: 'When you enable this setting, you will be able to import all packages (also nodejs pkgs) in Electron Isolated Content. Plugins can import all packages regardless of this setting. This setting is only for your security.',
+                                        type: 'warn'
+                                    },
+                                    (accepted) => {
+                                        if (accepted) resolve(settings.toggleSetting("hykord.bypass_security_check_require"));
+                                        else resolve(false);
+                                    }
+                                )
+                            })
+                        }}
                         note="Bypass security check require in window.require - plugins already have access to all modules"
                         value={settings.getSetting("hykord.bypass_security_check_require", false)}
                         label="Bypass Security Check Require"
-                        postHandle={() => openRequiredRestartModal()}
                     />
                     <Boolean
                         toggle={() => settings.toggleSetting("hykord.enable_dev_experiment_mod")}
