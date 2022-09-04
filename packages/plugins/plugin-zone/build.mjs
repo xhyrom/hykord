0f5787c5217b62d2c443f377fa6ae5e4d9ef92a3
Hyro <generalkubo@gmail.com>
2022-09-04 21:06:56 +0200

    fix(plugins): mark path as external

--- packages/plugins/plugin-zone/build.mjs
+++ packages/plugins/plugin-zone/build.mjs
@@ -7,7 +7,7 @@ try {
     minify: true,
     bundle: true,
     format: "iife",
-    external: ["@hykord/webpack", "@hykord/components", "@hykord/structures", "@hykord/utilities", "@hykord/patcher"],
+    external: ["@hykord/webpack", "@hykord/components", "@hykord/structures", "@hykord/utilities", "@hykord/patcher", "path"],
     target: ["esnext"],
   });
 
