0c920362537ce817010df2508f1d83fdb13a4785
xHyroM <generalkubo@gmail.com>
2022-10-09 19:06:10 +0200

    feat(apis/Settings): add unregisterSection

--- src/renderer/modules/apis/settings.ts
+++ src/renderer/modules/apis/settings.ts
@@ -1,15 +1,19 @@
 // Sections are injected in settingsApi plugin
-export const sections: { priority: number; section: string; label: string; element: any }[] = [];
+export let sections: { priority: number; section: string; label: string; element: any }[] = [];
 
 // For plugin settings use `settings` property in Plugin
 export const registerSection = (id: string, name: string, component: any, priority = 99) => {
-    const section = { priority: priority, section: id, label: name, element: component };
-    sections.push(section);
+  const section = { priority: priority, section: id, label: name, element: component };
+  sections.push(section);
 
-    sections.sort((a, b) => a.priority - b.priority);
+  sections.sort((a, b) => a.priority - b.priority);
 
-    return () => {
-      const i = sections.indexOf(section);
-      if (i !== -1) sections.splice(i, 1);
-    };
-}
+  return () => {
+    const i = sections.indexOf(section);
+    if (i !== -1) sections.splice(i, 1);
+  };
+};
+
+export const unregisterSection = (id: string) => {
+  sections = sections.filter((s) => s.section !== id);
+};
