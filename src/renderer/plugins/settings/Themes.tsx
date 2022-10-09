0d0d92a24a7ce112c71019975ce1b3c959bb95ae
xHyroM <generalkubo@gmail.com>
2022-10-09 12:25:10 +0200

    fix: add forceUpdate into Themes

--- src/renderer/plugins/settings/Themes.tsx
+++ src/renderer/plugins/settings/Themes.tsx
@@ -1,14 +1,17 @@
+import { React } from '@hykord/webpack';
 import { Forms } from '@hykord/components';
 import { ErrorBoundary } from '@hykord/components';
 import AddonCard from './cards/AddonCard';
 import { themes } from '@loader/theme';
 
 export default ErrorBoundary.wrap(() => {
+  const forceUpdate = React.useReducer((x) => x + 1, 0)[1];
+
   return (
     <Forms.FormSection tag="h1" title="Themes">
       <Forms.FormTitle>Here you can see installed themes</Forms.FormTitle>
       {themes.map((theme) => (
-        <AddonCard name={theme.name} type={'theme'} />
+        <AddonCard name={theme.name} type={'theme'} forceUpdate={forceUpdate} />
       ))}
     </Forms.FormSection>
   );
