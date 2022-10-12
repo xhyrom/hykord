58ecbe9cd8b3cba3db45844b97f58f6373c4c9dd
xHyroM <generalkubo@gmail.com>
2022-10-12 18:03:31 +0200

    feat(installer): add args parsing

---
+++ packages/installer/src/args.rs
@@ -0,0 +1,32 @@
+use clap::Parser;
+
+#[allow(non_camel_case_types)]
+#[derive(clap::ValueEnum, Clone, Debug)]
+pub enum Action {
+    install,
+    uninstall,
+    inject,
+    uninject,
+    repair
+}
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+pub struct Args {
+    // Action
+    #[clap(
+        value_enum,
+        required=true
+    )]
+    pub action: Action,
+
+    #[arg(
+        required=false
+    )]
+    pub path: Option<String>,
+}
+
+pub fn parse() -> Args {
+    let args = Args::parse();
+    return args;
+}
--- packages/installer/src/main.rs
+++ packages/installer/src/main.rs
@@ -1,3 +1,25 @@
+mod args;
+
 fn main() {
-    println!("Hello, world!");
+    let args = args::parse();
+
+    println!("{:?}", args);
+
+    match args.action {
+        args::Action::install => {
+            println!("Installing...");
+        }
+        args::Action::uninstall => {
+            println!("Uninstalling...");
+        }
+        args::Action::inject => {
+            println!("Injecting...");
+        }
+        args::Action::uninject => {
+            println!("Uninjecting...");
+        }
+        args::Action::repair => {
+            println!("Repairing...");
+        }
+    }
 }
