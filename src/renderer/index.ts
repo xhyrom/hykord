4f2251a35fddd01ff2cb664f4b87c369afd0fece
xHyroM <generalkubo@gmail.com>
2022-10-22 10:29:47 +0200

    feat: add Hykord.Modules

--- src/renderer/index.ts
+++ src/renderer/index.ts
@@ -17,10 +17,15 @@ export const gitHash = $HYKORD_GIT_HASH;
 export const directory = HykordNative.getDirectory();
 export const Settings = HykordNative.getManagers().getSettings();
 
+export * as Loaders from './loaders';
+
+// Modules
+export * as Modules from './modules';
+
+// Exported modules top level
 export * as Utils from '@hykord/utils';
 export * as Webpack from '@hykord/webpack';
 export * as Components from '@hykord/components';
 export * as Hooks from '@hykord/hooks';
 export * as Apis from '@hykord/apis';
 export * as Patcher from '@hykord/patcher';
-export * as Loaders from './loaders';
---
+++ src/renderer/modules/index.ts
@@ -0,0 +1,6 @@
+export * as Apis from './apis';
+export * as Components from './components';
+export * as Hooks from './hooks';
+export * as Patcher from './patcher';
+export * as Utils from './utils';
+export * as Webpack from './webpack';
